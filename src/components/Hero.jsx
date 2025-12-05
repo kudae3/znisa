fa942dd886aeb9496c345426b8a9ddc049694d35
Kudae Sithu <kudaesithu2@gmail.com>
2025-12-05 16:30:28 +0630

    feat: add background image to Hero component for enhanced visual appeal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import GradientButton from '../assets/ui/GradientBtn'
+import backgroundImage from '../assets/background.png'
 
 const Hero = () => {
   return (
-    <div className='text-white h-[567px] bg-gray-700'>
+    <div className='text-white h-[567px] relative bg-cover bg-center bg-no-repeat' style={{
+      backgroundImage: `url(${backgroundImage})`
+    }}>
         <div className='text-center mx-auto w-full max-w-[882px] h-[166px] flex flex-col gap-[11px] px-[175px] pt-[117px]'>
             <h2 className='font-normal text-[32px] leading-14'>I design and build clean websites</h2>
             <p className='font-normal text-[16px] leading-[23px]'>Write anything here something about yourself to showcase what actually you doing or targeting etc.</p>
