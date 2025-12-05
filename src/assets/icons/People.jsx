00654774a6c7ceeac72a8a57fac8ea4196d28822
Kudae Sithu <kudaesithu2@gmail.com>
2025-12-05 20:32:14 +0630

    feat: update SVG icons for consistency in attribute casing and improve visual styling

--- src/assets/icons/People.jsx
+++ src/assets/icons/People.jsx
@@ -2,8 +2,8 @@ const People = () => {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="20" height="20" stroke="url(#paint0_linear_1_71)"/>
-        <circle cx="9.99999" cy="5.00008" r="3.33333" stroke="black" stroke-width="1.5"/>
-        <ellipse cx="9.99999" cy="14.1666" rx="5.83333" ry="3.33333" stroke="black" stroke-width="1.5"/>
+        <circle cx="9.99999" cy="5.00008" r="3.33333" stroke="black" strokeWidth="1.5"/>
+        <ellipse cx="9.99999" cy="14.1666" rx="5.83333" ry="3.33333" stroke="black" strokeWidth="1.5"/>
     </svg>
   )
 }
